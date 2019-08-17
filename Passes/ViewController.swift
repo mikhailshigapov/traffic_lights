13cb9afc767bbce4a0c2ad0420d5ca5f91e6180f
Mikhail Shigapov <m.shigapov@list.ru>
2019-08-17 18:11:46 +0300

    Initial Commit

---
+++ Passes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Passes
+//
+//  Created by Mikhail Shigapov on 17/08/2019.
+//  Copyright © 2019 Mikhail Shigapov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
